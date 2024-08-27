04a5149149d0fc9d80860c3193900081ee657d51
KirKiller <KirKiller@DESKTOP-1JIA97R>
2024-08-27 22:30:35 +0500

    Добавьте файлы проекта.

---
+++ ДЗ контроль выхода/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ДЗ_контроль_выхода
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             int specifiedLiftDepth;
+            Console.Write("Введите с какой глубины необходимо осуществить подьём: ");
+            specifiedLiftDepth = Convert.ToInt32(Console.ReadLine());
+
+            while (specifiedLiftDepth-- > 0) 
+            {
+               Console.WriteLine($"Груз поднялся на {specifiedLiftDepth} метров");
+
+                if (specifiedLiftDepth == 7)
+                {
+                    Console.WriteLine("Лифт застрял!!");
+                        break;
+                }
+            }
+
+             
+
+
+
+
+        }
+    }
+}
