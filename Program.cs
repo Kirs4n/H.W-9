f264d2962a672956b9163440eb58884d017ec721
Kirs4n <165835810+Kirs4n@users.noreply.github.com>
2024-08-31 21:17:58 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ДЗ_контроль_выхода
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             bool login=true;
+            string commandExit = "exit";
+            string userInput;
+            
+            while (login) 
+            {
+               Console.WriteLine($"Произведен вход в систему");
+               userInput = Console.ReadLine();
+
+                if (commandExit==userInput)
+                {
+                    login = false;
+                    Console.WriteLine("Выход из системы");
+                        break;
+                }
+            }
+        }
+    }
+}
